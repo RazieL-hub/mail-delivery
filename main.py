835bc18529c3c90c18f68f6ff95dea68cb5bdc53
AntonSachuk <sachuk.aa@intechs.by>
2022-04-14 23:43:06 +0300

    test fastapi first start

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from fastapi import FastAPI
+import datetime
+
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+
+from apps.report.schemas import ReportSchema
+from core.celery import celery_app
+from database.async_connect_postgres import Session, get_session
+from apps.report.querys import create_task
 
 app = FastAPI()
 
@@ -8,6 +16,14 @@ async def root():
     return {'message': 'hello world'}
 
 
-@app.get("/")
-async def test_mailer_delivery():
+@app.get('/test_mailer_sent')
+async def test_mailer_delivery(db: Session = Depends(get_session)):
+    query = text(create_task).bindparams(**ReportSchema().dict())
+    task = await db.execute(query)
+    await db.commit()
+    last_task = task.first()
+    task_name = "core.celery.test_celery"
+    celery_app.sent_report(task_name, args=[last_task.id],
+                           eta=datetime.datetime.now() + datetime.timedelta(seconds=30))
+
     return {'message': 'Mailer delivery started'}
