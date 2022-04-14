3fa702098aee6ff0b747c70a833043429ab6dfcb
AntonSachuk <sachuk.aa@intechs.by>
2022-04-14 23:42:49 +0300

    add celery_task sent_report

---
+++ core/celery.py
@@ -0,0 +1,16 @@
+from celery import Celery
+from sqlalchemy import text
+
+from database.sync_connect_postgres import SessionLocal
+from apps.report.querys import update_task_status
+
+celery_app = Celery("worker", backend="rpc://user:bitnami@rabbitmq:5672//",
+                    broker="amqp://user:bitnami@rabbitmq:5672//")
+celery_app.conf.update(task_track_started=True)
+
+
+@celery_app.task(acks_late=True)
+def sent_report(task_id: int):
+    with SessionLocal() as db:
+        db.execute(text(update_task_status).bindparams(task_id=task_id))
+        db.commit()
